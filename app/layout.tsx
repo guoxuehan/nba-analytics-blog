5bd4e3fb145c57c29a0c6cdf5c109dd5361b058b
guoxuehan <pooh.s.xuehan@gmail.com>
2026-03-21 23:44:23 +0900

    Add: Search Console verification, sitemap, Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Oswald, Source_Sans_3, Noto_Sans_JP } from 'next/font/google'
+import Script from 'next/script'
 import { Providers } from './providers'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
@@ -60,6 +61,22 @@ export default function RootLayout({
           </main>
           <Footer />
         </Providers>
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+              `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   )
